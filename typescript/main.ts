dfb2e69c4a58b69e10f59d4c81b38a389c975571
wheath <wgheath@gmail.com>
2012-11-07 13:11:19 -1000

    turning everything into a rule

--- typescript/main.ts
+++ typescript/main.ts
@@ -31,14 +31,23 @@ var q_human = new Rule('human');
 //re.addRule(mortal_rule);
 re.addRule(human_rule);
 
+var human_rule2 = new Rule('human');
+var term_X2 = new Term('X');
+human_rule2.addArg(term_X2);
+var assignX2 = new Rule('X=aristotle');
+human_rule2.addRule(assignX2);
+re.addRule(human_rule2);
+
 //var query1 = new Query('mortal');
 //var a2 = new Atom('aristotle');
 var a2 = new Atom('socrates');
 //query1.addArg(a2);
-q_human.addArg(a2);
+//q_human.addArg(a2);
+q_human.addArg(term_X);
 //var query_result = re.query(query1);
 re.fireRule(q_human);
 //re.query(query1);
 console.log("_dbg term_X.grounded: " + term_X.grounded + "\n");
 console.log("_dbg q_human.args[0].name: " + q_human.args[0].name + "\n");
-console.log("_dbg query_result: " + q_human.proven + "\n");
+console.log("_dbg proof query_result: " + q_human.proven + "\n");
+console.log("_dbg solutions query_result: " + JSON.stringify(q_human.solutions) + "\n");
